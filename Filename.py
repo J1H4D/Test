3675fd476add1fe66ea83d5c7146e68969f2f278
J1H4D <40457497+J1H4D@users.noreply.github.com>
2018-10-24 21:45:45 +0700

    Add files via upload

---
+++ Filename.py
@@ -0,0 +1,11 @@
+username = raw_input("masukkan username : ")
+password = raw_input("masukkan password : ")
+username_from_db = "J106"
+password_from_db = "J1H4D"
+if username == username_from_db :
+ if password == password_from_db :
+    print "Username dan password cocok "
+ else:
+    print "Password salah "
+else:
+ print "User tidak terdaftar"
